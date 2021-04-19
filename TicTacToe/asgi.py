e50d9fa418f4a15e386966be76325e91ed442a31
egorius <petrenkoeggg@gmail.com>
2021-04-20 01:15:40 +0300

    FINAL FIX

--- TicTacToe/asgi.py
+++ TicTacToe/asgi.py
@@ -17,11 +17,13 @@
 #     ),
 # })
 
+from .wsgi import *
 import os
 import django
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TicTacToe.settings')
-django.setup()
 application = get_default_application()
+django.setup()
+
 
