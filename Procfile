022e315ea514b389473bfdf66aa87283512f3f33
egorius <petrenkoeggg@gmail.com>
2021-04-20 00:47:12 +0300

    asd

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: daphne -b 0.0.0.0 -p 8001 TicTacToe.asgi:application
-worker: python manage.py runworker --settings=TicTacToe.settings -v2 channel_layer
+worker: python manage.py runworker --settings=TicTacToe.settings channel_layer
--- TicTacToe/settings.py
+++ TicTacToe/settings.py
@@ -23,16 +23,16 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'channels',
-    'bootstrap4',
-    'django_tagify',
     'django.contrib.admin',
-    # 'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'channels',
+    'bootstrap4',
+    'django_tagify',
 ]
 
 MIDDLEWARE = [
