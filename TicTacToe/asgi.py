f203a9500a2f11ecb5077b7d15e5d744d72eb938
egorius <petrenkoeggg@gmail.com>
2021-04-19 22:36:51 +0300

    change asgi file

--- TicTacToe/asgi.py
+++ TicTacToe/asgi.py
@@ -4,14 +4,18 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import main.routing
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TicTacToe.settings')
 
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            main.routing.websocket_urlpatterns
-        )
-    ),
-})
+# application = ProtocolTypeRouter({
+#   "http": get_asgi_application(),
+#   "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             main.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
+
+django.setup()
+application = get_asgi_application()
