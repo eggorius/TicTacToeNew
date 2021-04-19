f99f98342412809b5a0ec95b2556fb24a0f0d6aa
egorius <petrenkoeggg@gmail.com>
2021-04-19 22:26:05 +0300

    Changed procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn TicTacToe.wsgi
+web: daphne TicTacToe.asgi:application --port $PORT --bind 0.0.0.0 -v2
+TicTacToeworker: python manage.py runworker --settings=TicTacToe.settings -v2
--- TicTacToe/settings.py
+++ TicTacToe/settings.py
@@ -71,10 +71,13 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            # "hosts": [('127.0.0.1', 6379)],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')]
         },
     },
 }
+
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
